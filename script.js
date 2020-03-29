4da10bc9067071cbbd298cc1e9013ed8d5f117d2
kelvinmuthama <kelvinkamau387@gmail.com>
2020-03-29 20:11:19 +0300

    Added js script for functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+search_btn.onclick = function () {
+
+    // Get the youtube url source
+    let url = document.getElementById("youtube-url").value; // Our iframe
+    let video_src = document.getElementById("video_src").src;
+
+    // This function is for changing the youtube url source on the iframe
+    function changeSrc() {
+        video_src = "https://www.youtube.com/embed/" + url[2];
+        document.getElementById("video-src").src = video_src;
+    }
+
+    if (url.match(/(\?|&)v=([^&#]+)/)) {
+        url = url.match(/(\?|&)v=([^&#]+)/);
+        changeSrc();
+    }
+}
